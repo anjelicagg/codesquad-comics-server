b6d90407bc12af593a7ce47ef9c6158b263b4e1e
anjelicagg <agg269@email.vccs.edu.com>
2025-06-17 14:21:51 -0400

    reviewed with Kit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const app= express()
+const PORT=8080
+
+//MIDDLEWARE//
+
+const morgan = require("morgan")
+const helmet = require("helmet")
+const cors = require("cors")
+const path= require("node:path")
+
+app.use(morgan("combined"))
+app.use(helmet)
+app.use(cors)
+
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.static(path.join(__dirname+"/public")))
+//ROUTES
+app.get("/",(request,response)=>{
+    // response.send("This route points to the Home page")
+    response.status().json({success:{message:"Success"},
+      
+    })
+});
+
+app.get("/api/books",(request,response)=>{
+    // response.send("This will send all of the book data")
+    response.status().json({
+        success:{message:"Success"},
+    })
+});
+
+app.get("/api/books/:id",(request,response)=>{
+    // response.send("This will update a book by its id")
+    response.status().json({
+        success:{message:"Success"},
+    })
+});
+app.get("/api/books/delete/:id",(request,response)=>{
+    // response.send("This will delete a book by its id")
+    response.status().json({
+        success:{message:"Success"},
+    })
+});
+
+app.listen(PORT,()=>{
+    console.log(`The server is listening on http://localhost:3000/ ${PORT}`)
+})

291bb4b1f7ff233ccfc7044994618e6654399bc9
anjelicagg <agg269@email.vccs.edu.com>
2025-06-18 23:37:37 -0400

    updates

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,23 +1,32 @@
+//ROUTES
 const express = require("express")
 const router =express.router()
-const { getAllbooks,getBook,createBook,updateBook,deleteBook }  = require ("../controllers/bookController");
 
-//ROUTES
-
-app.get("/api/books",(request,response,next)=>{
+app.get("/api/books",(request,response)=>{
     // response.send("This route points to the Home page")
-    response.status().json({success:{message:"Success"},
-      
-    })
+    response.status().json({success:{message:"This will send all of the book data"}})
 });
 
 
-app.post("/create/new",(createBook)=>{
+
+app.post("/create/new",(request,response)=>{
+    // response.send("This will delete a book by its id")
+    response.status().json({
+        success:{message:"Success"},
+    })
+});
+app.put("/update/:id",(request,response)=>{
+    // response.send("This will delete a book by its id")
+    response.status().json({
+        success:{message:"Success"},
+    })
+});
+app.delete("/delete/:id",(request,response)=>{
     // response.send("This will delete a book by its id")
     response.status().json({
         success:{message:"Success"},
     })
 });
 
-module.exports(createBook,)
+
 

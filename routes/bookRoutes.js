6f5544188a5b62ee50ffb124b3c37d52ca1e3a25
anjelicagg <agg269@email.vccs.edu.com>
2025-06-18 23:44:38 -0400

    updates

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,47 +1,31 @@
 //ROUTES
 const express = require("express")
-const router =express.router()
-app.get("/",(request,response)=>{
+const router =express.Router()
+
+app.get("/api/books",(request,response,next)=>{
     // response.send("This route points to the Home page")
-    response.status().json({success:{message:"Success"},
-      
-    })
+    response.status().json({success:{message:"This will send all of the book data"}})
 });
 
-app.get("/.",(request,response)=>{
-    // response.send("This will send all of the book data")
-    response.status().json({
-        success:{message:"Success"},
-    })
-});
 
-app.get("/:id",(request,response)=>{
-    // response.send("This will update a book by its id")
-    response.status().json({
-        success:{message:"Success"},
-    })
-});
 
-app.post("/create/new",(request,response)=>{
+app.post("/create/new",(request,response,next)=>{
     // response.send("This will delete a book by its id")
     response.status().json({
         success:{message:"Success"},
     })
 });
-app.put("/update/:id",(request,response)=>{
+app.put("/update/:id",(request,response,next)=>{
     // response.send("This will delete a book by its id")
     response.status().json({
-        success:{message:"Success"},
+        success:{message:"This will delete a book by its id"},
     })
 });
 app.delete("/delete/:id",(request,response)=>{
     // response.send("This will delete a book by its id")
     response.status().json({
-        success:{message:"Success"},
+        success:{message:"This will delete a book by its id"},
     })
 });
 
-app.listen(PORT,()=>{
-    console.log(`The server is listening on http://localhost:3000/ ${PORT}`)
-})
-
+module.exports(Router)

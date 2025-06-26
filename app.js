b152ae39173b1f0827bee0d1e42f330a9fcdaeb4
anjelicagg <agg269@email.vccs.edu.com>
2025-06-26 17:11:48 -0400

    cp-server-3-part-1

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const connectDB=require("./config/connection")
 const express = require("express")
 const app= express()
 const PORT=8080
@@ -18,14 +20,38 @@ app.use(cors())
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-app.use(express.static(path.join(__dirname+"/public")))
-//ROUTES
-app.get("/",(request,response)=>{
-    // response.send("This route points to the Home page")
-    response.status().json({success:{message:"This route points to the Home page"},
-      
-    })
+app.use(express.static(path.join(__dirname+"/public")));
+
+app.use(session({
+    resave:false,
+    saveUninitialized:false,
+    secret:process.env.SECRET_KEY,
+    COOKIE:{
+        httpOnly:true,
+        secure:false,
+        maxAge:1000*60*60*24,
+    },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("api/book",bookRoutes);
+app.use("/auth",authRoutes);
+
+app.use((err,req,res,next)=>{
+    if (err.code ===11000){
+return res.status(err.status || 400).json({
+    error:{message:"Already have an account? Try logging in."},
+    statusCode: err.status || 400,
 });
+}
+return res.status(err.status ||500).json({
+    error: {message:err.message || "Internal server error."},
+    statusCode: err.status || 500,
+});
+})
+//ROUTES
+
 
 //CP-SERVER-2 PT-1//
 app.use("/route/bookRoutes", (req,res,)=>{
@@ -35,10 +61,25 @@ app.use("/route/bookRoutes", (req,res,)=>{
 
 });
 
-app.use("/api",authRoutes)
-app.listen(PORT,()=>{
-    console.log(`The server is listening on http://localhost: ${PORT}`)
+app.get("/",(req,res,next)=>{
+    res.status(200).json({
+        success:{message:"Index page works after deployment"},
+        statusCode:200,
+    });
 });
+async function startServer() {
+    try{
+        await connectDB();
+
+    
+
+app.listen(PORT,()=>
+    console.log("MongoDB database is connected")
+);
+}catch(error){
+    console.error(`There was an error connecting to the database: ${error}`);
+}
+};
 
 
-module.exports()
+startServer()
